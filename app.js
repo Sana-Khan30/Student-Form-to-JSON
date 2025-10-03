95d1a24c1237f08da75d6364ad5459ff4982d523
Sana khan <sanasamad592@gmail.com>
2025-10-03 14:54:17 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const students = [];
+let stdInfo = document.getElementById('stdInfo')
+stdInfo.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let name = document.getElementById('name').value;
+    let age = document.getElementById('age').value;
+    let id = document.getElementById('id').value;
+    let AdmissionDate = document.getElementById('Date').value;
+    let Batch = document.getElementById('Batch').value;
+
+    if(name && age && id && AdmissionDate && Batch){
+        const student = {
+            name ,
+            age ,
+            id ,
+            AdmissionDate ,
+            Batch
+        };
+        students.push(student);
+        stdInfo.reset();
+        stdInfo(alert('form successfully submited ✔'));
+    }
+
+    else{
+        alert('please fill out all fields');
+    }
+})
+document.getElementById('showData').addEventListener('click', () => {
+    const jsonData = JSON.stringify(students, null, 2);
+    document.getElementById('output').innerHTML = `<pre>${jsonData}</pre>`;
+})
